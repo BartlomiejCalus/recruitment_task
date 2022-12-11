76ae81a03ab5e73e53ab6ad926e46b78fcebfc68
Bartłomiej Całus <bartlomiej.calus01@gmail.com>
2022-12-11 16:18:05 +0100

    little change in mapping

---
+++ myFunction/myFunction/Entities/Output/CustomExtraFields.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace myFunction.Entities.Output
+{
+    public class CustomExtraFields
+    {
+        [JsonProperty("135")]
+        public string _135 { get; set; }
+
+        [JsonProperty("172")]
+        public string _172 { get; set; }
+    }
+}
